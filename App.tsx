b19129fe64693579d4966010f076e6f08a485ae4
Filip Kinský <filip@filovo.net>
2025-02-27 14:40:33 +0100

    add controls for toggling `symbolSortKey` in style and increasing priority for one specific unimportant POI to be able to simulate prioritization and performance issues

--- App.tsx
+++ App.tsx
@@ -1,9 +1,10 @@
 import { Camera, CameraRef, Images, MapView, MapViewRef, ShapeSource, SymbolLayer, SymbolLayerStyle } from '@maplibre/maplibre-react-native';
-import React, { useMemo, useRef, useState } from 'react';
+import React, { useCallback, useMemo, useRef, useState } from 'react';
 import pois from './pois';
+import { Button, StyleProp, Switch, Text, TouchableHighlight, View, ViewProps, ViewStyle } from 'react-native';
 
 const baseStyle: SymbolLayerStyle = {
-  symbolSortKey: ['get', 'priority'], // comment this out to increase performance
+  //symbolSortKey: ['get', 'priority'], // comment this out to increase performance
   textHaloWidth: 2,
   textHaloBlur: 0,
   textAnchor: 'top',
@@ -16,31 +17,11 @@ const baseStyle: SymbolLayerStyle = {
   textOffset: [0, 0.1],
 };
 
-const pinLargeStyle: SymbolLayerStyle = {
-  ...baseStyle,
-  textField: ['get', 'title'],
-  textSize: 14,
-  textColor: ['get', 'textColor'],
-  textHaloColor: ['get', 'textHaloColor'],
-  iconImage: 'poi_L',
-};
-
-const pinMediumStyle: SymbolLayerStyle = {
-  ...baseStyle,
-  iconImage: 'poi_M',
-};
-
-
-const pinSmallStyle: SymbolLayerStyle = {
-  iconImage: 'poi_S',
-  iconAnchor: 'center',
-  iconSize: 1 / 2,
-};
-
 function App(): React.JSX.Element {
   const camera = useRef<CameraRef>(null);
   const map = useRef<MapViewRef>(null);
   const [cameraPosition] = useState<number[]>([14.4285631, 50.0806125]);
+  const [prioPoiId, setPrioPoiId] = useState<string|undefined>();
 
   const images = {
     poi_S: require('./icons/poi-s.png'),
@@ -52,31 +33,88 @@ function App(): React.JSX.Element {
     const features: GeoJSON.FeatureCollection =
     {
       type: 'FeatureCollection',
-      features: pois.map(p => ({
-        type: 'Feature',
-        id: p.id,
-        properties: {
-          title: p.name,
-          priority: 100 - p.priority,
-          textColor: 'black',
-          textHaloColor: 'white',
-        },
-        geometry: {
-          type: 'Point',
-          coordinates: [p.latitude, p.longitude],
-        },
-      })),
+      features: pois.map(p => {
+        const priority = p.id === prioPoiId ? 0 : 100 - p.priority;
+        return ({
+          type: 'Feature',
+          id: p.id,
+          properties: {
+            title: `${p.name} (${priority.toFixed(2)})`,
+            priority,
+            textColor: p.id === prioPoiId ? 'white' : 'black',
+            textHaloColor: p.id === prioPoiId ? 'red' : 'white',
+          },
+          geometry: {
+            type: 'Point',
+            coordinates: [p.latitude, p.longitude],
+          },
+        } as GeoJSON.Feature);
+      })
+      .sort((a, b) => a.properties?.priority - b.properties?.priority)
+      ,
     };
 
     return features;
+  }, [prioPoiId]);
+
+  const style = useMemo(() => ({ flex: 1, backgroundColor: 'white' } as StyleProp<ViewStyle>), []);
+  const toggleToiletsAsTop = useCallback(() => {
+    if (prioPoiId === undefined) {
+      setPrioPoiId('sygic-poi-283376');
+    }
+    else {
+      setPrioPoiId(undefined);
+    }
+  }, [prioPoiId]);
+
+  const onFeaturePress = useCallback((e: {features: GeoJSON.Feature[]}) => {
+    console.log('feature pressed', e.features[0]?.id);
   }, []);
 
-  const style = useMemo(() => ({ flex: 1 }), []);
+  const [pinLargeStyle, setPinLargeStyle] = useState<SymbolLayerStyle>({
+    ...baseStyle,
+    textField: ['get', 'title'],
+    textSize: 14,
+    textColor: ['get', 'textColor'],
+    textHaloColor: ['get', 'textHaloColor'],
+    iconImage: 'poi_L',
+  });
+
+  const [pinMediumStyle, setPinMediumStyle] = useState<SymbolLayerStyle>({
+    ...baseStyle,
+    iconImage: 'poi_M',
+  });
+
+  const [pinSmallStyle, setPinSmallStyle] = useState<SymbolLayerStyle>({
+    iconImage: 'poi_S',
+    iconAnchor: 'center',
+    iconSize: 1 / 2,
+  });
+
+  const [useSymbolSortKey, setUseSymbolSortKey] = useState<boolean>(false);
+  const onSymbolSortKeyToggle = useCallback(() => {
+    const newValue = !useSymbolSortKey;
+    setUseSymbolSortKey(newValue);
+    if (newValue) {
+      setPinLargeStyle({...pinLargeStyle, symbolSortKey: ['get', 'priority']});
+      setPinMediumStyle({...pinMediumStyle, symbolSortKey: ['get', 'priority']});
+      setPinSmallStyle({...pinSmallStyle, symbolSortKey: ['get', 'priority']});
+    }
+    else {
+      /* eslint-disable @typescript-eslint/no-unused-vars */
+      setPinLargeStyle(({ symbolSortKey, ...rest }) => rest);
+      setPinMediumStyle(({ symbolSortKey, ...rest }) => rest);
+      setPinSmallStyle(({ symbolSortKey, ...rest }) => rest);
+      /* eslint-enable @typescript-eslint/no-unused-vars */
+    }
+  }, [pinLargeStyle, pinMediumStyle, pinSmallStyle, useSymbolSortKey]);
 
   return (
+    <View style={style}>
     <MapView
       ref={map}
       style={style}
+      mapStyle="https://demotiles.maplibre.org/styles/osm-bright-gl-style/style.json"
     >
       <Camera ref={camera}
         centerCoordinate={cameraPosition}
@@ -85,7 +123,7 @@ function App(): React.JSX.Element {
       <Images images={images} />
       <ShapeSource
         id="layer1"
-        shape={featuresCollection}
+        shape={featuresCollection} onPress={onFeaturePress}
       >
         <SymbolLayer
           id="small"
@@ -101,7 +139,19 @@ function App(): React.JSX.Element {
         />
       </ShapeSource>
     </MapView>
-  );
+    <View>
+      <Button title={ prioPoiId === undefined ? 'Set toilets as high prio' : 'Reset toilets priority'} onPress={toggleToiletsAsTop} />
+      <TouchableHighlight onPress={onSymbolSortKeyToggle}>
+        <View style={{flexDirection: 'row', justifyContent: 'space-around' }}>
+          <Text style={{verticalAlign: 'middle'}}>use symbolSortKey in style</Text>
+          <Switch style={{marginVertical: 16}} value={useSymbolSortKey} onValueChange={onSymbolSortKeyToggle} />
+        </View>
+      </TouchableHighlight>
+      <Text style={{marginVertical: 6}}>{`1st feature: ${featuresCollection.features[0].properties?.title}`}</Text>
+      <Text>{JSON.stringify(pinLargeStyle)}</Text>
+    </View>
+    </View>
+      );
 }
 
 export default App;
