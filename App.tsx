86fbac0f3cf75c3504336cb215356ad59e96e6b1
Filip Kinský <filip@filovo.net>
2025-02-27 16:17:20 +0100

    add toggles for switching between style configurations (static vs useState-powered)

--- App.tsx
+++ App.tsx
@@ -1,7 +1,7 @@
 import { Camera, CameraRef, Images, MapView, MapViewRef, ShapeSource, SymbolLayer, SymbolLayerStyle } from '@maplibre/maplibre-react-native';
 import React, { useCallback, useMemo, useRef, useState } from 'react';
 import pois from './pois';
-import { Button, StyleProp, Switch, Text, TouchableHighlight, View, ViewProps, ViewStyle } from 'react-native';
+import { Button, StyleProp, Switch, Text, TouchableHighlight, View, ViewStyle } from 'react-native';
 
 const baseStyle: SymbolLayerStyle = {
   //symbolSortKey: ['get', 'priority'], // comment this out to increase performance
@@ -17,10 +17,34 @@ const baseStyle: SymbolLayerStyle = {
   textOffset: [0, 0.1],
 };
 
+const staticPinLargeStyle: SymbolLayerStyle = {
+  ...baseStyle,
+  textField: ['get', 'title'],
+  textSize: 14,
+  textColor: ['get', 'textColor'],
+  textHaloColor: ['get', 'textHaloColor'],
+  iconImage: 'poi_L',
+};
+
+const staticPinMediumStyle: SymbolLayerStyle = {
+  ...baseStyle,
+  iconImage: 'poi_M',
+};
+
+
+const staticPinSmallStyle: SymbolLayerStyle = {
+  iconImage: 'poi_S',
+  iconAnchor: 'center',
+  iconSize: 1 / 2,
+};
+
+const pragueLocation = [14.4285631, 50.0806125];
+const innsbruckLocation = [11.3824194, 47.2648672];
+
 function App(): React.JSX.Element {
   const camera = useRef<CameraRef>(null);
   const map = useRef<MapViewRef>(null);
-  const [cameraPosition] = useState<number[]>([14.4285631, 50.0806125]);
+  const [cameraPosition] = useState<number[]>(innsbruckLocation);
   const [prioPoiId, setPrioPoiId] = useState<string|undefined>();
 
   const images = {
@@ -29,7 +53,11 @@ function App(): React.JSX.Element {
     poi_L: require('./icons/poi-l.png'),
   };
 
+
   const featuresCollection: GeoJSON.FeatureCollection = useMemo(() => {
+    const latitudeShift = innsbruckLocation[0] - pragueLocation[0];
+    const longitudeShift = innsbruckLocation[1] - pragueLocation[1];
+
     const features: GeoJSON.FeatureCollection =
     {
       type: 'FeatureCollection',
@@ -46,7 +74,7 @@ function App(): React.JSX.Element {
           },
           geometry: {
             type: 'Point',
-            coordinates: [p.latitude, p.longitude],
+            coordinates: [p.latitude + latitudeShift, p.longitude + longitudeShift],
           },
         } as GeoJSON.Feature);
       })
@@ -69,6 +97,9 @@ function App(): React.JSX.Element {
 
   const onFeaturePress = useCallback((e: {features: GeoJSON.Feature[]}) => {
     console.log('feature pressed', e.features[0]?.id);
+    if (e.features[0]?.id){
+      setPrioPoiId(`${e.features[0]?.id}`);
+    }
   }, []);
 
   const [pinLargeStyle, setPinLargeStyle] = useState<SymbolLayerStyle>({
@@ -96,6 +127,7 @@ function App(): React.JSX.Element {
     const newValue = !useSymbolSortKey;
     setUseSymbolSortKey(newValue);
     if (newValue) {
+      setUseStaticStyle(false);
       setPinLargeStyle({...pinLargeStyle, symbolSortKey: ['get', 'priority']});
       setPinMediumStyle({...pinMediumStyle, symbolSortKey: ['get', 'priority']});
       setPinSmallStyle({...pinSmallStyle, symbolSortKey: ['get', 'priority']});
@@ -109,12 +141,26 @@ function App(): React.JSX.Element {
     }
   }, [pinLargeStyle, pinMediumStyle, pinSmallStyle, useSymbolSortKey]);
 
+  const [useStaticStyle, setUseStaticStyle] = useState<boolean>(false);
+
+  const onStaticStyleToggle = useCallback(() => {
+    const newValue = !useStaticStyle;
+    setUseStaticStyle(newValue);
+    if (newValue){
+      setUseSymbolSortKey(false);
+    }
+  }, [useStaticStyle]);
+
+  console.log('rerender');
+
   return (
     <View style={style}>
+      <Text style={{margin: 6, fontSize: 18}}>Tap a POI to select it, tap the map to deselect</Text>
     <MapView
       ref={map}
       style={style}
       mapStyle="https://demotiles.maplibre.org/styles/osm-bright-gl-style/style.json"
+      onPress={useCallback(() => {setPrioPoiId(undefined);}, [])}
     >
       <Camera ref={camera}
         centerCoordinate={cameraPosition}
@@ -127,24 +173,30 @@ function App(): React.JSX.Element {
       >
         <SymbolLayer
           id="small"
-          style={pinSmallStyle}
+          style={useStaticStyle ? staticPinSmallStyle : pinSmallStyle}
         />
         <SymbolLayer
           id="medium"
-          style={pinMediumStyle}
+          style={useStaticStyle ? staticPinMediumStyle : pinMediumStyle}
         />
         <SymbolLayer
           id="large"
-          style={pinLargeStyle}
+          style={useStaticStyle ? staticPinLargeStyle : pinLargeStyle}
         />
       </ShapeSource>
     </MapView>
     <View>
-      <Button title={ prioPoiId === undefined ? 'Set toilets as high prio' : 'Reset toilets priority'} onPress={toggleToiletsAsTop} />
+      <Button title={ prioPoiId === undefined ? 'Set toilets as high prio' : 'Reset priority'} onPress={toggleToiletsAsTop} />
       <TouchableHighlight onPress={onSymbolSortKeyToggle}>
         <View style={{flexDirection: 'row', justifyContent: 'space-around' }}>
           <Text style={{verticalAlign: 'middle'}}>use symbolSortKey in style</Text>
           <Switch style={{marginVertical: 16}} value={useSymbolSortKey} onValueChange={onSymbolSortKeyToggle} />
+        </View>
+      </TouchableHighlight>
+      <TouchableHighlight onPress={onStaticStyleToggle}>
+        <View style={{flexDirection: 'row', justifyContent: 'space-around' }}>
+          <Text style={{verticalAlign: 'middle'}}>use static style</Text>
+          <Switch style={{marginVertical: 16}} value={useStaticStyle} onValueChange={onStaticStyleToggle} />
         </View>
       </TouchableHighlight>
       <Text style={{marginVertical: 6}}>{`1st feature: ${featuresCollection.features[0].properties?.title}`}</Text>
